873065684480deecead7583931e84a1251e4be1d
trivuniceva <trivunic99@gmail.com>
2024-10-15 00:14:04 +0200

    .

---
+++ src/main/java/bookingApp/model/User.java
@@ -0,0 +1,134 @@
+package bookingApp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private UserRole userRole;
+
+    @Column(name = "reset_token")
+    private String resetToken;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "is_blocked")
+    private boolean isBlocked;
+
+    @Column(name = "profile_pic")
+    private String profilePic;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+}
---
+++ src/main/java/bookingApp/service/UserService.java
@@ -0,0 +1,102 @@
+package bookingApp.service;
+
+import bookingApp.dto.UserDTO;
+import bookingApp.model.User;
+import bookingApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public User loggedUser;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            return null;
+        }
+        if (user.getPassword().equals(password)) {
+            System.out.println("dobra sifra");
+            return user;
+        }
+        return null;
+    }
+
+//    public ResponseEntity<String> sendPasswordResetEmail(String email) {
+//        User user = userRepository.findByEmail(email);
+//        if (user != null) {
+//            String token = generateResetToken(user);
+//
+//            user.setResetToken(token);
+//            userRepository.save(user);
+//            System.out.println("Token saved: " + token);
+//
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom("imenkoprezimic123@gmail.com");
+//            message.setTo(email);
+//            message.setSubject("Password Reset Request");
+//            message.setText("To reset your password, click the link below:\n\n" +
+//                    "http://localhost:4200/reset-password?token=" + token);
+//            mailSender.send(message);
+//
+//            return ResponseEntity.ok("Password reset email sent.");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+//        }
+//    }
+
+    private String generateResetToken(User user) {
+        return UUID.randomUUID().toString();
+    }
+
+    public User findByResetToken(String token) {
+        System.out.println("Looking for user with token: " + token);
+        User user = userRepository.findByResetToken(token);
+        if (user == null) {
+            System.out.println("No user found with token: " + token);
+        }
+        return user;
+    }
+
+    public User save(User user) {
+        try {
+            return userRepository.save(user);
+        } catch (Exception e) {
+            // Log the error
+            System.err.println("Error saving user: " + e.getMessage());
+            throw e; // or handle it appropriately
+        }
+    }
+
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+    public void updateUser(UserDTO userDto) {
+        User user = userRepository.findByEmail(userDto.getEmail());
+
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        user.setFirstname(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setProfilePic(user.getProfilePic());
+
+        userRepository.save(user);
+
+    }
+}
