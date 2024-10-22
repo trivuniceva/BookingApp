7e3da57541fe5e6657d13e8d84b26d10490434b3
trivuniceva <trivunic99@gmail.com>
2024-10-22 23:39:59 +0200

    add role

--- frontend/src/app/core/service/auth/auth.service.ts
+++ frontend/src/app/core/service/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import {BehaviorSubject, catchError, Observable, throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private userRoleSubject = new BehaviorSubject<string>('');
+  userRole$ = this.userRoleSubject.asObservable();
 
   private apiUrl = 'http://localhost:8080/api/auth/login';
 
@@ -13,6 +15,22 @@ export class AuthService {
 
   login(email: string, password: string): Observable<any> {
     console.log("uso u log")
+
+    let user = localStorage.getItem('user');
+    if (user) {
+      console.log("rola: ")
+      let parsedUser = JSON.parse(user);
+      console.log(parsedUser.userRole);
+
+      this.userRoleSubject.next(parsedUser.userRole);
+    }
+
+
+    // this.userRoleSubject.next()
+
+    console.log("uso u log")
+
+
     return this.http.post(this.apiUrl, { email, password });
   }
 
