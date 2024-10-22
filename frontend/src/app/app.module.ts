c8a93f6bf8255f5ce93dbf90a22d6c9c5ef1ee7f
trivuniceva <trivunic99@gmail.com>
2024-10-22 18:39:47 +0200

    valjda radi sad joj

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthService} from './service/auth/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [AuthService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from '../service/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    console.log("ajmo log")
+    this.authService.login(this.email, this.password).subscribe(response => {
+      console.log('Login successful', response);
+      localStorage.setItem('user', JSON.stringify(response)); // Čuvaj korisnika
+    }, error => {
+      console.error('Login failed', error);
+    });
+  }
+
+  forgotPassword() {
+
+  }
+
+  goToSignup() {
+    this.router.navigate(['/signup'])
+  }
+}
