76459e8bd39ad1052cec2124999a532548ba38f9
trivuniceva <trivunic99@gmail.com>
2024-10-22 23:40:39 +0200

    add role

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../core/service/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  userRole: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+
+    this.authService.userRole$.subscribe(role => {
+      this.userRole = role;
+    });
+
+
+
+  }
 }
